b641d3f2821b28c25a5fe9fd0d7a4949cb20e701
yaho <rlatjwls2923@gmail.com>
2024-05-21 14:06:16 +0900

    Feat: First Commit

---
+++ android/app/src/main/kotlin/com/example/bocket_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bocket_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main_view_model.dart
@@ -0,0 +1,23 @@
+import 'package:bocket_test/social_login.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+
+class MainViewModel{
+  final SocialLogin _socialLogin;
+  bool isLogined = false;
+  User? user;
+
+  MainViewModel(this._socialLogin);
+
+  Future login() async{
+    isLogined = await _socialLogin.login();
+    if(isLogined){
+      user = await UserApi.instance.me();
+      print("로그인 성공. 유저 정보는 = ${user!.kakaoAccount}");
+    }
+  }
+  Future logout() async{
+    await _socialLogin.logout();
+    isLogined = false;
+    user = null;
+  }
+}
