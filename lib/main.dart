faaa1f33c7fe811a8d622353580794de836b5d6c
yaho <rlatjwls2923@gmail.com>
2024-07-03 16:38:40 +0900

    feat: onBoarding page 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bocket_test/LoginPage.dart';
 import 'package:bocket_test/home_page/main_page.dart';
 import 'package:bocket_test/kakao_login.dart';
 import 'package:bocket_test/main_view_model.dart';
+import 'package:bocket_test/onBoarding3.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
@@ -10,8 +11,8 @@ import 'onBoarding2.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env'); // Required by FlutterConfig
-  var appKey = dotenv.env['nativeAppKey'];
-  KakaoSdk.init(nativeAppKey: appKey);
+  var appKey = dotenv.env['jsAppKey'];
+  KakaoSdk.init(javaScriptAppKey: appKey);
   runApp(const MyApp());
 }
 
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Waaam',
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
-      home: MyHomePage(),
+      home: onBoard3(),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
--- lib/onBoarding3.dart
+++ lib/onBoarding3.dart
@@ -3,7 +3,8 @@ import 'home_page/main_page.dart';
 import 'onBoarding2.dart';
 
 class onBoard3 extends StatefulWidget {
-  final String? nickname; // Define nickname as a parameter
+  final String? nickname;
+
   const onBoard3({this.nickname});
 
   @override
@@ -11,6 +12,15 @@ class onBoard3 extends StatefulWidget {
 }
 
 class _onBoard3State extends State<onBoard3> {
+  List<bool> selectedGoals = [false, false, false, false, false];
+  bool get isAnyGoalSelected => selectedGoals.contains(true);
+
+  void toggleSelection(int index) {
+    setState(() {
+      selectedGoals[index] = !selectedGoals[index];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -42,40 +52,78 @@ class _onBoard3State extends State<onBoard3> {
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
               ),
               Text(
-                "${widget.nickname}님의 목표를 알려주세요!",
+                "${widget.nickname ?? ''}님의 목표를 알려주세요!",
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
               ),
-              SizedBox(height: 16,),
-              TextButton(onPressed: (){},
+              SizedBox(height: 12),
+              TextButton(
+                onPressed: () {},
                 child: Row(
                   children: [
-                    Icon(Icons.create_outlined, size: 10,color: Color(0xFF747B85),),
-                    Text("직접 입력할게요",
+                    Icon(Icons.create_outlined, size: 18, color: Color(0xFF747B85)),
+                    SizedBox(width: 4),
+                    Text(
+                      "직접 입력할게요",
                       style: TextStyle(
-                        color:Color(0xFF747B85),
+                        color: Color(0xFF747B85),
                         fontSize: 12,
                         fontWeight: FontWeight.w500,
                         decoration: TextDecoration.underline,
-                        decorationColor:Color(0xFF747B85),
+                        decorationColor: Color(0xFF747B85),
                       ),
-                    ),],
-                )
+                    ),
+                  ],
+                ),
               ),
+              SizedBox(height: 20),
+              Wrap(
+                spacing: 20.0,
+                runSpacing: 20.0,
+                children: [
+                  Goal(
+                    text: "매일매일 공부",
+                    selected: selectedGoals[0],
+                    onTap: () => toggleSelection(0),
+                  ),
+                  Goal(
+                    text: "A+을 향해서",
+                    selected: selectedGoals[1],
+                    onTap: () => toggleSelection(1),
+                  ),
+                  Goal(
+                    text: "자격증 합격",
+                    selected: selectedGoals[2],
+                    onTap: () => toggleSelection(2),
+                  ),
+                  Goal(
+                    text: "시험공부는 꼼꼼히",
+                    selected: selectedGoals[3],
+                    onTap: () => toggleSelection(3),
+                  ),
+                  Goal(
+                    text: "일상 속 복습 습관 만들기",
+                    selected: selectedGoals[4],
+                    onTap: () => toggleSelection(4),
+                  ),
+                ],
+              ),
+              Expanded(child: Container()), // Fills up the remaining space
               Center(
                 child: GestureDetector(
-                  onTap: (){Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => MainPage()),
-                  );},
+                  onTap: isAnyGoalSelected
+                      ? () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainPage()),
+                    );
+                  }
+                      : null,
                   child: Container(
                     alignment: Alignment.center,
-                    width: 290,
+                    width: double.infinity,
                     height: 60,
                     decoration: BoxDecoration(
-                      color:
-                      // _isButtonEnabled?
-                        Color(0xFF02B6B4),
-                          // : Color(0xFFBABFCA),
+                      color: isAnyGoalSelected ? Color(0xFF02B6B4) : Color(0xFFBABFCA),
                       borderRadius: BorderRadius.circular(12),
                     ),
                     child: Text(
@@ -87,8 +135,50 @@ class _onBoard3State extends State<onBoard3> {
                     ),
                   ),
                 ),
-              )
+              ),
+              SizedBox(height: 20), // Adds spacing below the button
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Goal extends StatelessWidget {
+  final String text;
+  final bool selected;
+  final VoidCallback onTap;
+
+  const Goal({
+    required this.text,
+    required this.selected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: IntrinsicWidth(
+        child: Container(
+          height: 40,
+          padding: EdgeInsets.fromLTRB(14, 8, 14, 8),
+          decoration: BoxDecoration(
+            color: selected ? Color(0xFFE9F9F8) : Color(0xFFEEEEEF),
+            borderRadius: BorderRadius.circular(18),
+            border: selected ? Border.all(
+              color: Color(0xFF8AE5E4), width: 1) : null
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: selected ? Color(0xFF02B6B4) : Color(0xFF747B85),
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ),
         ),
       ),
