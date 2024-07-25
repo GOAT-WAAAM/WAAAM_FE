ecbe3c70b45181364230645a67026854e9a4e4f9
yaho <rlatjwls2923@gmail.com>
2024-07-25 15:06:04 +0900

    design: on boarding css

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Waaam',
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
-      home: MyHomePage(),
+      home: onBoard1(),
     );
   }
 }
